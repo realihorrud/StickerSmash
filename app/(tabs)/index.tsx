6a92ce6e8f4d3b3f70c64baa851cc9dc8d8575de
Ihor Rud <Igorryd7@gmail.com>
2026-02-17 11:49:05 +0200

    Complete add navigation, build a screen, use an image picker, create a modal lessons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,101 @@
-import { Text, View, StyleSheet } from "react-native";
-import {Link} from "expo-router";
+import {View, StyleSheet, ImageSourcePropType} from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from "react";
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
+
+const PlaceholderImage = require("@/assets/images/background-image.png")
 
 export default function Index() {
-  return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Home screen</Text>
-        <Link href="/about" style={styles.button}>Go to About screen</Link>
-    </View>
-  );
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined)
+    const [showAppOptions, setShowAppOptions] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            quality: 1
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+            setShowAppOptions(true);
+        } else {
+            alert('You did not select any image.');
+        }
+    }
+
+    const onReset = () => {
+        setShowAppOptions(false);
+    }
+
+    const onAddSticker = () => {
+        setIsModalVisible(true);
+    }
+
+    const onSaveImageAsync = () => {
+
+    }
+
+    const onModalClose = () => {
+        setIsModalVisible(false);
+    }
+
+    return (
+        <View
+            style={styles.container}
+        >
+            <View style={styles.imageContainer}>
+                <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
+                {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>}
+            </View>
+            {showAppOptions ? (
+                <View style={styles.optionsContainer}>
+                    <View style={styles.optionsRow}>
+                        <IconButton label="Reset" onPress={onReset} icon="refresh"/>
+                        <CircleButton onPress={onAddSticker}/>
+                        <IconButton label="Save" onPress={onSaveImageAsync} icon="save-alt"/>
+                    </View>
+                </View>
+            ) : (
+                <View style={styles.footerContainer}>
+                    <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
+                    <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
+                </View>
+            )}
+
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+                <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+            </EmojiPicker>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#25292e",
-        justifyContent: "center",
         alignItems: "center",
     },
-    text: {
-        color: "#fff",
-        fontWeight: "900",
+    imageContainer: {
+        flex: 1,
     },
-    button: {
-        fontSize: 20,
-        textDecorationLine: "underline",
-        color: "#fff",
-    }
+    footerContainer: {
+        flex: 1 / 3,
+    },
+    optionsContainer: {
+        position: 'absolute',
+        bottom: 80,
+    },
+    optionsRow: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
 })
